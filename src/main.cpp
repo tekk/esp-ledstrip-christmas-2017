cded360cecd83a81bc3a3586dd869c62baae597a
Peter Javorsky <peter.javorsky@inloop.eu>
2018-01-20 01:45:34 +0100

    FIrst attempt for OTA updates.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,21 @@ Date: 13.12.2017
 
 #include <Arduino.h>
 #include <FastLED.h>
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>          
+#else
+#include <WiFi.h>          
+#endif
+
+//needed for library
+#include <DNSServer.h>
+#if defined(ESP8266)
+#include <ESP8266WebServer.h>
+#else
+#include <WebServer.h>
+#endif
+#include <WiFiManager.h>
+#include <ArduinoOTA.h>
 
 // How many leds are in the strip?
 #define NUM_LEDS 50
@@ -33,7 +48,7 @@ const uint16_t MAX_FPS = 60;
 const int EFFECTS_COUNT = 1;
 
 // effects variables
-uint8_t max_bright = 128;
+uint8_t max_bright = 255;
 uint8_t thisdelay = 40;
 uint8_t thishue = 0;
 int8_t thisrot = 1;
@@ -41,8 +56,10 @@ uint8_t deltahue = 1;
 bool thisdir = 0;
 bool isChangemeSet = false;
 
+WiFiManager wifiManager;
+
 void rainbow_march() {                                        // The fill_rainbow call doesn't support brightness levels. You would need to change the max_bright value.
-  if (thisdir == 0) thishue += thisrot; else thishue-= thisrot;  // I could use signed math, but 'thisdir' works with other routines.
+  if (thisdir == 0) thishue += thisrot; else thishue -= thisrot;  // I could use signed math, but 'thisdir' works with other routines.
   fill_rainbow(leds, NUM_LEDS, thishue, deltahue);            // I don't change deltahue on the fly as it's too fast near the end of the strip.
 }
 
@@ -54,25 +71,27 @@ void taskLedStrip(void *parameter)
     uint8_t secondHand;
     static uint8_t lastSecond = 99;
 
-      switch (effect)
-      {
-          case 0:
-              speedDivider = 50;
-              i = (xTaskGetTickCount() * portTICK_PERIOD_MS / speedDivider) % NUM_LEDS;
-              leds[i] = CRGB::White;
-              if (i > 0) {
-                 leds[i - 1] = CRGB::Black;
-              }
-              FastLED.show();
-              FastLED.delay(100);
-          break;
+    effect = (millis() / 60000) % 2;
 
-          case 1:
+    switch (effect)
+    {
+        case 0:
+            speedDivider = 50;
+            i = (int)round(sin(millis() / 1000.0) * (xTaskGetTickCount() * portTICK_PERIOD_MS / speedDivider)) % NUM_LEDS;
+            leds[i] = CRGB::White;
+            if (i > 0) {
+                leds[i - 1] = CRGB::Black;
+            }
+            FastLED.show();
+            FastLED.delay(50);
+        break;
+
+        case 1:
             secondHand = ((xTaskGetTickCount() * portTICK_PERIOD_MS) / 1000) % 60;                // Change '60' to a different value to change length of the loop.
 
             if (lastSecond != secondHand) {                             // Debounce to make sure we're not repeating an assignment.
-              lastSecond = secondHand;
-              switch(secondHand) {
+                lastSecond = secondHand;
+                switch(secondHand) {
                 case  0: thisrot=1; deltahue=5; break;
                 case  5: thisdir=-1; deltahue=10; break;
                 case 10: thisrot=5; break;
@@ -80,16 +99,16 @@ void taskLedStrip(void *parameter)
                 case 20: deltahue=30; break;
                 case 25: deltahue=2; thisrot=5; break;
                 case 30: break;
-              }
+                }
             }
 
             rainbow_march();
             FastLED.show();
             FastLED.delay(thisdelay);
-          break;
+        break;
 
-          default:
-          break;
+        default:
+        break;
       }
     }
 
@@ -97,11 +116,40 @@ void taskLedStrip(void *parameter)
 }
 
 void setupWifi() {
+    wifiManager.autoConnect("LedStripAP");
 
+    ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void handleWifi() {
-
+    ArduinoOTA.handle();
 }
 
 void taskWifi(void *parameter)
@@ -115,14 +163,14 @@ void taskWifi(void *parameter)
 
 void setup() {
 	// sanity check delay - allows reprogramming if accidently blowing power w/leds
-  // 	delay(2000);
+    delay(2000);
 
     Serial.begin(115200);
 
     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-    FastLED.setMaxRefreshRate(MAX_FPS);
-    FastLED.setBrightness(max_bright);
-    set_max_power_in_volts_and_milliamps(5, 500);
+    //FastLED.setMaxRefreshRate(MAX_FPS);
+    //FastLED.setBrightness(max_bright);
+    //set_max_power_in_volts_and_milliamps(5, 500);
 
     pinMode(LED_BUILTIN, OUTPUT);
 
